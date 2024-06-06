4c97c386f2ed07757981625f2b2ac75b5a95a4b4
pedrovcg <pedrovcg@outlook.com>
2024-06-06 08:23:49 -0300

    desafio-contador

---
+++ DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner terminal = new Scanner (System.in);
+        
+        System.out.println("Digite o primeiro número inteiro");
+        int num1 = terminal.nextInt();
+
+        System.out.println("Digite o segundo número inteiro");
+        int num2 = terminal.nextInt();
+
+        try {
+            //chamando o método
+            contar(num1, num2);
+
+        } catch (ParametrosInvalidosException e) {   
+            // Mensagem de erro para exceções
+            System.out.println("O segundo número deve ser maior que o primeiro");
+
+        }
+    }
+
+    static void contar (int num1, int num2) throws ParametrosInvalidosException {
+
+        if (num1 > num2) {
+            throw new ParametrosInvalidosException();
+        }
+
+        int contagem = num2 - num1;
+            for (contagem = contagem; contagem <= num2; contagem++) {
+                System.out.println("Imprimindo " + contagem);
+        }
+    }
+}
